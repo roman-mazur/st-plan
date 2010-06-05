f0532db392fac1ff305dcf0f28ccc49a04476ce2
mazur.roman <mazur.roman@9914fb8d-2203-891a-f882-100260acfeb4>
2010-06-05 17:42:03 +0000

    statistics collector

--- trunk/src/main/groovy/org/mazur/stplan/StatisticsCollector.groovy
+++ trunk/src/main/groovy/org/mazur/stplan/StatisticsCollector.groovy
@@ -32,7 +32,7 @@ State.INSTANCE.sysParams = systemEditor.systemParameters
 def processors = State.INSTANCE.processorsAlg(systemEditor.parent, systemEditor.model)[0]
 systemEditor = null                   
 
-def genParametesrs = new GenerationParameteres(
+def genParameters = new GenerationParameteres(
   minVertexWeight : vertexWeightRange.from,
   maxVertexWeight : vertexWeightRange.to,
   n : n
@@ -40,38 +40,49 @@ def genParametesrs = new GenerationParameteres(
 
 def out = new FileWriter(output)
 
-def serializeResult = { def result, long time, int gNumber, int maxTime ->
-  def timeF = { x -> x.task.duration.end}
-  
-  int algTime = result.max { a,b -> a.task}
+out << "k\t"
+out << "Задача\t"
+combinations.eachWithIndex { c, i -> out << "$i\t\t\t\t\t" }
+out << "\r\n"
+
+def serializeResult = { def result, long time, int gNumber, int maxTime, float k, int algNumber ->
+  def timeF = { x -> x.task.duration.end.time }
+  int algTime = timeF(result.max { a, b -> timeF(a) <=> timeF(b) })
+  float ka = maxTime / algTime
+  if (!algNumber) { out << "$k\t$gNumber\t" }
+  out << "$ka\tn/a\tn/a\t$time\t"
+  if (algNumber == combinations.size() - 1) { out << "\r\n" }
+  out.flush()
 }
 
 9.times {
   println "-----------------------------------------------"
   float k = (it + 1) * 0.1
   println "Generation for ${k}"
-  6.times {
+  10.times {
     int graphNumber = it + 1
     println "GRAPH ${graphNumber}"
     genParameters.k = k 
     def graphEditor = new TaskGraphEditor(name : "auto")
     new GraphGenerator(editor : graphEditor, parameters : genParameters).run()
-    int totalGraphTime = ModelUtil.getTotalGraphTime(graphEditor.model, graphEditor.parent)
+    int totalGraphTime = new ModelUtil().getTotalGraphTime(graphEditor.parent, graphEditor.model)
     println "Generation finished $totalGraphTime"
     
-    combinations.each {
-      State.INSTANCE.queueAlg = it[0]
-      State.INSTANCE.selectProcessorAlg = it[1]
+    combinations.eachWithIndex { c, algNumber ->
+      State.INSTANCE.queueAlg = c[0]
+      State.INSTANCE.selectProcessorAlg = c[1]
       println "Simulate for ${State.INSTANCE}"
       def simManager = new SimManager(simState : new SimState(
         freeProcessors : processors,
-        tasksQueue : State.INSTANCE.queueAlg.formQueue(graphEditor.parent, graphEditor.model)                   
+        tasksQueue : State.INSTANCE.queueAlg.formQueue(graphEditor.parent, graphEditor.model)[0]                   
       ))
       long startTime = System.currentTimeMillis()
       def result = simManager.all()
       long time = System.currentTimeMillis() - startTime
       println "Time: $time"
-      serializeResult(result, time, graphNumber, totalGraphTime)
+      serializeResult(result, time, graphNumber, totalGraphTime, k, algNumber)
     }
   }
 }
+
+out.close()
